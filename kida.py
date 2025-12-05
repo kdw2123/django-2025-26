33c9af254dd6699e632c485d93d20bbb2fc8ef85
kidan <kid.wdk@gmail.com>
2025-12-05 07:37:12 -0500

    msg task 1

---
+++ kida.py
@@ -0,0 +1,5 @@
+items = int(input("enter the number of items you want : "))
+if items >= 3: 
+    print("you have get discount ")
+else:
+    print("no discount for you ")
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-try :
-    with open ("data.txt","r")as file:
-        content = file.read()
-        print (content)
-except:
-    with open ("data.txt ","w" )as file:
-        content= file.write("hi ther ")
-        print (content)
+price = [120,45,300,85,150]
+def get_expensive_price(price):
+  expensive = []
+
+  for i in price:
+    if i >= 100:
+      
+      expensive.append(i)
+      
+  return expensive 
+print(get_expensive_price(price))
+
+    
+      
